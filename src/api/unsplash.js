2bff43b6966b9fcce3871fe7d107d61bbc67e984
charlie38 <38466672+charlie38@users.noreply.github.com>
2024-01-13 16:20:36 +0100

    :alien: Update API key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-      'Client-ID tCo20q-iQtI8Tf2aU0Vp07aI1K7Sy6yYraTKcDCY-cI'
+      'Client-ID M4QJStmJaZrCssgbjPzgclkD3Co5v09oi7qiZEC6EkY'
   }
 });
